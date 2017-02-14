c71070ead64afcef0abca5a90071d2cb1e8a280f
dennisuid <dennis.uid@gmail.com>
2017-02-14 11:19:41 +0000

    Update function added, untested Get function added, tested. Need to add JSON serialisation

--- src/Shift/EmployerBundle/Controller/EmployerController.php
+++ src/Shift/EmployerBundle/Controller/EmployerController.php
@@ -32,25 +32,54 @@ class EmployerController extends Controller
         }
     }
 
-    public function updateAction()
+    public function updateAction(Request $request)
     {
-        return $this->render('EmployerBundle:Employer:update.html.twig', array(
-                        // ...
-        ));
+        try {
+            $user = new fysUser();
+            $user->setFirstName($request->get('first_name'));
+            $user->setFirstName($request->get('last_name'));
+            $user->setEmail($request->get('email'));
+            $user->setMobileNumber($request->get('mobile_number'));
+            $user->setUserId($request->get('userid'));
+            $user->setFirstName($request->get('user_type'));
+            $user->setPassword($request->get('password'));
+            $user->setUserType($request->get('house_number'));
+            $user->setFirstName($request->get('address_line1'));
+            $user->setFirstName($request->get('address_line2'));
+            $user->setPostcode($request->get('postcode'));
+            $user->setCountry($request->get('country'));
+            $user->setRegistrationNumber($request->get('registration_number'));
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+            return new Response("Data Saved Successfully");
+        } catch(\Exception $ex){
+            return new Response($ex->getMessage());
+        }
     }
 
-    public function deleteAction()
+    public function getAction(Request $request)
     {
-        return $this->render('EmployerBundle:Employer:delete.html.twig', array(
-                        // ...
-        ));
+         $user = $this->getDoctrine()
+                 ->getRepository('EmployerBundle:User\fysUser')
+                 ->find($request->get('id'));
+         
+         return new Response($user->getFirstName());
+    }
+    
+    public function deleteAction(Request $request)
+    {
+        $user = new fysUser();
+        $user->setFirstName($request->get('user_id'));
+        // how to write delete code into DB
     }
 
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('EmployerBundle:Employer:login.html.twig', array(
-                        // ...
-        ));
+        $user = new fysUser();
+        $user->setFirstName($request->get('email'));
+        $user->setFirstName($request->get('password'));
+        // how to write delete code into DB
     }
 
 }
