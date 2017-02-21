d213598803b302f32daaa987e126932805f8883a
Riya Dennis <riyacv@gmail.com>
2017-02-21 11:05:52 +0000

    Added a new bundle for the fronend

--- src/Shift/ShiftBundle/Controller/ShiftUserController.php
+++ src/Shift/ShiftBundle/Controller/ShiftUserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\HttpFoundation\Response;
 use Shift\ShiftBundle\Exception\UserNotFoundException;
+use Shift\ShiftBundle\Form\User\fysUserType;
 
 class ShiftUserController extends Controller
 {
@@ -23,8 +24,7 @@ class ShiftUserController extends Controller
         $encoders = array(new JsonEncoder());
         $normalizers = array(new ObjectNormalizer());
         if (!$user) {
-            throw new UserNotFoundException("Invalid user id " . $request->get('id'),
-            JsonResponse::HTTP_NOT_FOUND
+            throw new UserNotFoundException("Invalid user id " . $request->get('id'), JsonResponse::HTTP_NOT_FOUND
             );
         }
         $serializer = new Serializer($normalizers, $encoders);
@@ -50,32 +50,30 @@ class ShiftUserController extends Controller
 
     public function addAction(Request $request)
     {
-        $error = "";
         try {
             $user = new fysUser();
-            $user->setFirstName($request->get('first_name'));
-            $user->setEmail($request->get('email'));
-            $user->setMobileNumber($request->get('mobile_number'));
-            $user->setUserId($request->get('userid'));
-            $user->setPassword($request->get('password'));
-            $user->setUserType($request->get('user_type'));
-            $user->setPostcode($request->get('postcode'));
-            $user->setCountry($request->get('country'));
-            $user->setRegistrationNumber($request->get('registration_number'));
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($user);
-            $em->flush();
-            $success = " User added successfully";
+            $form = $this->createForm(fysUserType::class, $user);
+            $form->handleRequest($request);
+            $message = "";
+            if ($form->isSubmitted()) {
+                $user = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+                $message = 'User added successfully';
+                return new JsonResponse(['description' => $message],
+                        Response::HTTP_CREATED, 
+                        ['Content-type' => 'json']
+                );
+            }
         } catch (Exception $ex) {
-            $error = $ex->getMessage();
+            $message = $ex->getMessage() . " " . $ex->getCode();
         }
-
-        return $this->render('ShiftBundle:ShiftUser:add.html.twig', array(
-                    'error' => $error,
-                    'success' => $success
-        ));
+        return $this->render("ShiftBundle:ShiftUser:add.html.twig", [
+                    'form' => $form->createView()
+        ]);
     }
-
+    
     public function loginAction()
     {
         return $this->render('ShiftBundle:ShiftUser:login.html.twig', array(
