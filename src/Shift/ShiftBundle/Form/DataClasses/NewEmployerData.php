08e74ea9268ee5c102061a9d29b768eed810038b
riyadennis <riya.dennis@sainsburys.co.uk>
2017-03-05 18:52:22 +0000

    Front end bundle (#14)
    * Added a new bundle for the fronend
    
    * Added bootstarp theme to the form
    
    * Added home controller shift controller
    
    * Added bootstrap to my application

---
+++ src/Shift/ShiftBundle/Form/DataClasses/NewEmployerData.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Shift\ShiftBundle\Form\DataClasses;
+
+/**
+ * Class NewEmployerData
+ * @package Shift\ShiftBundle\Form\DataClasses
+ */
+class NewEmployerData
+{
+
+    private $first_name;
+    private $last_name;
+    private $email;
+    private $mobile_number;
+    private $user_id;
+    private $password;
+    private $user_type;
+    private $postcode;
+    private $registration_number;
+    private $country;
+    private $house_number;
+    private $address_line1;
+    private $address_line2;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMobileNumber()
+    {
+        return $this->mobile_number;
+    }
+
+    /**
+     * @param mixed $mobile_number
+     */
+    public function setMobileNumber($mobile_number)
+    {
+        $this->mobile_number = $mobile_number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserType()
+    {
+        return $this->user_type;
+    }
+
+    /**
+     * @param mixed $user_type
+     */
+    public function setUserType($user_type)
+    {
+        $this->user_type = $user_type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * @param mixed $postcode
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRegistrationNumber()
+    {
+        return $this->registration_number;
+    }
+
+    /**
+     * @param mixed $registration_number
+     */
+    public function setRegistrationNumber($registration_number)
+    {
+        $this->registration_number = $registration_number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHouseNumber()
+    {
+        return $this->house_number;
+    }
+
+    /**
+     * @param mixed $house_number
+     */
+    public function setHouseNumber($house_number)
+    {
+        $this->house_number = $house_number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddressLine1()
+    {
+        return $this->address_line1;
+    }
+
+    /**
+     * @param mixed $address_line1
+     */
+    public function setAddressLine1($address_line1)
+    {
+        $this->address_line1 = $address_line1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddressLine2()
+    {
+        return $this->address_line2;
+    }
+
+    /**
+     * @param mixed $address_line2
+     */
+    public function setAddressLine2($address_line2)
+    {
+        $this->address_line2 = $address_line2;
+    }
+}
